a129b2c72e494e005589b50f81fa84e0a4748cda
Mounir Ybanez <rinuom91@gmail.com>
2017-11-16 16:17:24 +0800

    Added Access model.

---
+++ Chika/Source/Models/Access.swift
@@ -0,0 +1,15 @@
+//
+//  Access.swift
+//  Chika
+//
+//  Created by Mounir Ybanez on 11/14/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+struct Access {
+
+    var token: String = ""
+    var userID: String = ""
+    var email: String = ""
+    var refreshToken: String = ""
+}
