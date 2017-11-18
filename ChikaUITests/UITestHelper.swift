a9ceb504a191d13846577c392b2765379ec66884
Mounir Ybanez <rinuom91@gmail.com>
2017-11-18 13:10:34 +0800

    Added UI test helper and SignIn UI tests.

---
+++ ChikaUITests/UITestHelper.swift
@@ -0,0 +1,12 @@
+//
+//  UITestHelper.swift
+//  ChikaUITests
+//
+//  Created by Mounir Ybanez on 11/18/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+enum LaunchArgument: String {
+    
+    case forceInitialScene = "ForceInitialScene"
+}
