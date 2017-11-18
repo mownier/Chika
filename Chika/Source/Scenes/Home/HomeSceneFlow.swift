194900894bf0def1e2f79b33ef7a5b9e15572d30
Mounir Ybanez <rinuom91@gmail.com>
2017-11-18 13:03:02 +0800

    Removed home scene flow logic.

--- Chika/Source/Scenes/Home/HomeSceneFlow.swift
+++ Chika/Source/Scenes/Home/HomeSceneFlow.swift
@@ -9,39 +9,12 @@
 import UIKit
 
 protocol HomeSceneFlow: class {
-    
-    func connect(from scene: UIViewController) -> Bool
+
 }
 
 extension HomeScene {
     
     class Flow: HomeSceneFlow {
         
-        var homeSceneFactory: HomeSceneFactory
-        var navControllerFactory: AppNavigationControllerFactory
-        
-        init(homeSceneFactory: HomeSceneFactory = HomeScene.Factory(),
-            navControllerFactory: AppNavigationControllerFactory = UINavigationController.Factory()) {
-            self.homeSceneFactory = homeSceneFactory
-            self.navControllerFactory = navControllerFactory
-        }
-        
-        func connect(from parent: UIViewController) -> Bool {
-            guard let window = parent.view.window else {
-                return false
-            }
-            
-            let scene = homeSceneFactory.build()
-            
-            guard let nav = window.rootViewController as? UINavigationController else {
-                let nav = navControllerFactory.build(root: scene)
-                window.rootViewController = nav
-                return true
-            }
-            
-            nav.popToRootViewController(animated: true)
-            nav.viewControllers[0] = scene
-            return true
-        }
     }
 }
