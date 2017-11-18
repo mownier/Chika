621c659446c3ca3a34e954a448b700869fe914a0
Mounir Ybanez <rinuom91@gmail.com>
2017-11-18 13:08:00 +0800

    Added exit and entry waypoints.

---
+++ Chika/Source/Scenes/Sign In/SignInSceneWaypoint.swift
@@ -0,0 +1,45 @@
+//
+//  SignInSceneWaypoint.swift
+//  Chika
+//
+//  Created by Mounir Ybanez on 11/17/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import UIKit
+
+extension SignInScene {
+    
+    class ExitWaypoint: AppExitWaypoint {
+        
+        weak var scene: UIViewController?
+        
+        func exit() -> Bool {
+            guard let scene = scene, let nav = scene.navigationController, nav.topViewController == scene, nav.viewControllers.count > 1 else {
+                return false
+            }
+            
+            nav.popViewController(animated: true)
+            return true
+        }
+    }
+    
+    class EntryWaypoint: AppEntryWaypoint {
+        
+        var sceneFactory: SignInSceneFactory
+        
+        init(sceneFactory: SignInSceneFactory = SignInScene.Factory()) {
+            self.sceneFactory = sceneFactory
+        }
+        
+        func enter(from parent: UIViewController) -> Bool {
+            guard let nav = parent.navigationController else {
+                return false
+            }
+            
+            let scene = sceneFactory.build()
+            nav.pushViewController(scene, animated: true)
+            return true
+        }
+    }
+}
