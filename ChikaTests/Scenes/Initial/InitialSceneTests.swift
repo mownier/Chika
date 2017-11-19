d2fd39321661ef562ac9c5ea03b5a2f4d400ac91
Mounir Ybanez <rinuom91@gmail.com>
2017-11-19 15:49:32 +0800

    Added tests on didTapSignIn and didTapRegister.

--- ChikaTests/Scenes/Initial/InitialSceneTests.swift
+++ ChikaTests/Scenes/Initial/InitialSceneTests.swift
@@ -23,4 +23,28 @@ class InitialSceneTests: XCTestCase {
         XCTAssertTrue(scene.theme is InitialScene.Theme)
         XCTAssertTrue(scene.flow is InitialScene.Flow)
     }
+    
+    // CONTEXT: didTapSignIn function should call the signIn function
+    // in flow given that the signIn button is tapped
+    func testDidTapSignInA() {
+        let theme = InitialScene.Theme()
+        let flow = InitialSceneFlowMock()
+        let scene = InitialScene(theme: theme, flow: flow)
+        let _ = scene.view
+        flow.isGoToSignInCalled = false
+        scene.signInButton.sendActions(for: .touchUpInside)
+        XCTAssertTrue(flow.isGoToSignInCalled)
+    }
+    
+    // CONTEXT: didTapRegister function should call the register function
+    // in flow given that the register button is tapped
+    func testDidTapRegisterA() {
+        let theme = InitialScene.Theme()
+        let flow = InitialSceneFlowMock()
+        let scene = InitialScene(theme: theme, flow: flow)
+        let _ = scene.view
+        flow.isGoToRegisterCalled = false
+        scene.registerButton.sendActions(for: .touchUpInside)
+        XCTAssertTrue(flow.isGoToRegisterCalled)
+    }
 }
