1d3185446f5d94bee1b851dd046f0b695f0a9d01
Mounir Ybanez <rinuom91@gmail.com>
2017-11-20 17:26:30 +0800

    Added remote service for Person.

---
+++ Chika/Source/Services/Remote/PersonRemoteService.swift
@@ -0,0 +1,36 @@
+//
+//  PersonRemoteService.swift
+//  Chika
+//
+//  Created by Mounir Ybanez on 11/20/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import FirebaseDatabase
+
+protocol PersonRemoteService: class {
+    
+    func add(email: String, id: String, completion: @escaping (ServiceResult<String>) -> Void)
+}
+
+class PersonRemoteServiceProvider: PersonRemoteService {
+
+    var database: Database
+    
+    init(database: Database = Database.database()) {
+        self.database = database
+    }
+    
+    func add(email: String, id: String, completion: @escaping (ServiceResult<String>) -> Void) {
+        let ref = database.reference()
+        let values = ["person/\(id)": ["email": email, "id": id]]
+        ref.updateChildValues(values) { error, _ in
+            guard error == nil else {
+                completion(.err(error!))
+                return
+            }
+            
+            completion(.ok("OK"))
+        }
+    }
+}
