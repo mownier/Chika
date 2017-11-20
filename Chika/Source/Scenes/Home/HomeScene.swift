3f56ee07ffef9e3e6638cb6e69c8ad4f6d1aad29
Mounir Ybanez <rinuom91@gmail.com>
2017-11-20 16:05:31 +0800

    Implemented HomeScene as UITabBarController.

--- Chika/Source/Scenes/Home/HomeScene.swift
+++ Chika/Source/Scenes/Home/HomeScene.swift
@@ -8,6 +8,34 @@
 
 import UIKit
 
-class HomeScene: UIViewController {
+class HomeScene: UITabBarController {
 
+    var theme: HomeSceneTheme!
+    var setup: HomeSceneTabSetup!
+    
+    init(theme: HomeSceneTheme, setup: HomeSceneTabSetup) {
+        self.theme = theme
+        self.setup = setup
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    convenience init() {
+        let theme = Theme()
+        let setup = TabSetup()
+        self.init(theme: theme, setup: setup)
+    }
+    
+    convenience required init?(coder aDecoder: NSCoder) {
+        self.init()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        tabBar.tintColor = theme.tabBarTintColor
+        tabBar.barTintColor = theme.tabBarBarTintColor
+        tabBar.isTranslucent = theme.tabBarIsTranslucent
+        
+        viewControllers = setup.subscenes
+    }
 }
