a5aee2e75dc73d71f3674062ecfb07689ce8b7c1
Mounir Ybanez <rinuom91@gmail.com>
2017-11-22 16:11:34 +0800

    Added mocks for DatabaseQuery and DataSnapshot.
    - Improved DatabaseReference mock.

--- ChikaTests/Mocks/FirebaseDatabaseMock.swift
+++ ChikaTests/Mocks/FirebaseDatabaseMock.swift
@@ -26,9 +26,14 @@ class FirebaseDatabaseMock: Database {
 class FirebaseDatabaseRefMock: DatabaseReference {
     
     var isOK: Bool
+    var query: FirebaseDatabaseQueryMock
+    var childPath: String?
+    var observerEventType: DataEventType?
+    var snapshotValues: [Any?] = []
     
     override init() {
         self.isOK = true
+        self.query = FirebaseDatabaseQueryMock()
         super.init()
     }
     
@@ -39,5 +44,57 @@ class FirebaseDatabaseRefMock: DatabaseReference {
         }
         
         block(nil, self)
+    }
+    
+    override func queryOrdered(byChild key: String) -> DatabaseQuery {
+        query.orderByChildKey = key
+        return query
+    }
+    
+    override func child(_ pathString: String) -> DatabaseReference {
+        childPath = pathString
+        return self
+    }
+    
+    override func observeSingleEvent(of eventType: DataEventType, with block: @escaping (DataSnapshot) -> Void) {
+        observerEventType = eventType
+        let snapshot = FirebaseDataSnapshot(value: snapshotValues.removeFirst())
+        block(snapshot)
+    }
+}
+
+class FirebaseDatabaseQueryMock: DatabaseQuery {
+ 
+    var orderByChildKey: String = ""
+    var limitToLast: UInt = 0
+    var observerEventType: DataEventType?
+    var snapshotValue: Any?
+    
+    override init() {
+        super.init()
+    }
+    
+    override func queryLimited(toLast limit: UInt) -> DatabaseQuery {
+        limitToLast = limit
+        return self
+    }
+    
+    override func observeSingleEvent(of eventType: DataEventType, with block: @escaping (DataSnapshot) -> Void) {
+        observerEventType = eventType
+        let snapshot = FirebaseDataSnapshot(value: snapshotValue)
+        block(snapshot)
+    }
+}
+
+class FirebaseDataSnapshot: DataSnapshot {
+    
+    var val: Any?
+    
+    override var value: Any? {
+        return val
+    }
+    
+    init(value: Any?) {
+        self.val = value
     }
 }
