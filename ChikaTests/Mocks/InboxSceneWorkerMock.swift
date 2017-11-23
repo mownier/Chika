1358773f3e57ffc86b4e4af7039123d0833dd55c
Mounir Ybanez <rinuom91@gmail.com>
2017-11-23 14:14:41 +0800

    Added mock on InboxSceneWorker.

---
+++ ChikaTests/Mocks/InboxSceneWorkerMock.swift
@@ -0,0 +1,17 @@
+//
+//  InboxSceneWorkerMock.swift
+//  ChikaTests
+//
+//  Created by Mounir Ybanez on 11/23/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import UIKit
+@testable import Chika
+
+class InboxSceneWorkerMock: InboxSceneWorker {
+
+    func fetchInbox() {
+        
+    }
+}
