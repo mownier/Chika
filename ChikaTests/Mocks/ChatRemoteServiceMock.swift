68b42938f454254cf01a43c46ab23d0c27b0f96a
Mounir Ybanez <rinuom91@gmail.com>
2017-11-23 14:13:59 +0800

    Added mock on ChatRemoteService.

---
+++ ChikaTests/Mocks/ChatRemoteServiceMock.swift
@@ -0,0 +1,25 @@
+//
+//  ChatRemoteServiceMock.swift
+//  ChikaTests
+//
+//  Created by Mounir Ybanez on 11/23/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+@testable import Chika
+
+class ChatRemoteServiceMock: ChatRemoteService {
+
+    var isForcedError: Bool = false
+    var chats: [Chat] = []
+    
+    func getInbox(for userID: String, completion: @escaping (ServiceResult<[Chat]>) -> Void) {
+        guard isForcedError else {
+            completion(.ok(chats))
+            return
+        }
+        
+        let error = ServiceError("ChatRemoteService forced error")
+        completion(.err(error))
+    }
+}
