d14408560c7ba4d01b9278bbdf283eac1fafb346
Mounir Ybanez <rinuom91@gmail.com>
2017-11-24 22:37:03 +0800

    Refactored to RootWaypoint.

--- Chika/Source/Scenes/Home/HomeSceneWaypoint.swift
+++ Chika/Source/Scenes/Home/HomeSceneWaypoint.swift
@@ -10,42 +10,21 @@ import UIKit
 
 extension HomeScene {
     
-    class EntryWaypoint: AppEntryWaypoint {
+    class RootWaypoint: AppRootWaypoint {
         
-        struct Factory {
-            
-            var homeScene: HomeSceneFactory
-            var navController: AppNavigationControllerFactory
+        var home: HomeSceneFactory
+        
+        init(home: HomeSceneFactory = HomeScene.Factory()) {
+            self.home = home
         }
         
-        var factory: Factory
-        
-        init(factory: Factory) {
-            self.factory = factory
-        }
-        
-        convenience init() {
-            let homeScene = HomeScene.Factory()
-            let navController = UINavigationController.Factory()
-            let factory = Factory(homeScene: homeScene, navController: navController)
-            self.init(factory: factory)
-        }
-        
-        func enter(from parent: UIViewController) -> Bool {
-            guard let window = parent.view.window else {
+        func makeRoot(from window: UIWindow?) -> Bool {
+            guard let window = window else {
                 return false
             }
             
-            let scene = factory.homeScene.build()
-            
-            guard let nav = window.rootViewController as? UINavigationController else {
-                let nav = factory.navController.build(root: scene)
-                window.rootViewController = nav
-                return true
-            }
-            
-            nav.popToRootViewController(animated: false)
-            nav.viewControllers[0] = scene
+            let scene = home.build()
+            window.rootViewController = scene
             return true
         }
     }
