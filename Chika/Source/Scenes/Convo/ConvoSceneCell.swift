6a8baf9d641628a0b35ffc1c269c9a145c62b312
Mounir Ybanez <rinuom91@gmail.com>
2017-11-25 16:06:16 +0800

    Checkpoint: Layouting ConvoScene cell.

--- Chika/Source/Scenes/Convo/ConvoSceneCell.swift
+++ Chika/Source/Scenes/Convo/ConvoSceneCell.swift
@@ -16,8 +16,11 @@ class ConvoSceneCell: UITableViewCell {
         case right = "CellForRight"
     }
     
+    var avatarImageView: UIImageView!
     var contentLabel: UILabel!
     var contentBGView: UIView!
+    var authorLabel: UILabel!
+    var timeLabel: UILabel!
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -42,8 +45,22 @@ class ConvoSceneCell: UITableViewCell {
         contentBGView.layer.cornerRadius = 4
         contentBGView.layer.masksToBounds = true
         
+        avatarImageView = UIImageView()
+        avatarImageView.backgroundColor = .gray
+        avatarImageView.layer.masksToBounds = true
+        
+        authorLabel = UILabel()
+        authorLabel.numberOfLines = 0
+        
+        timeLabel = UILabel()
+        timeLabel.numberOfLines = 0
+        timeLabel.textAlignment = reuseIdentifier == ReuseID.right.rawValue ? .right : .left
+        
         addSubview(contentBGView)
         addSubview(contentLabel)
+        addSubview(avatarImageView)
+        addSubview(authorLabel)
+        addSubview(timeLabel)
     }
     
     override func layoutSubviews() {
@@ -53,46 +70,83 @@ class ConvoSceneCell: UITableViewCell {
         
         let spacing: CGFloat = 8
         var rect = CGRect.zero
-        rect.origin.y = spacing * 4
         
         switch reuseID {
         case ReuseID.left.rawValue:
-            contentBGView.backgroundColor = .blue
-            contentLabel.textColor = .white
+            let avatarWidth: CGFloat = 32
             
-            rect.size.width = bounds.width * 0.8 - spacing * 4
-            rect.origin.x = spacing * 4
+            rect.origin.y = spacing
+            rect.origin.x = spacing
+            rect.size.width = avatarWidth
+            rect.size.height = rect.width
+            avatarImageView.frame = rect
+            avatarImageView.layer.cornerRadius = rect.width / 2
+            
+            rect.origin.x = rect.maxX + spacing
+            rect.size.width = (bounds.width - rect.origin.x - spacing) * 0.8
+            if authorLabel.isHidden {
+                rect.origin.y = -spacing
+                rect.size.height = 0
+            } else {
+                rect.size.height = authorLabel.sizeThatFits(rect.size).height
+            }
+            authorLabel.frame = rect
+            
+            rect.origin.y = rect.maxY + spacing * 2
+            rect.size.width -= (spacing * 2)
             rect.size.height = contentLabel.sizeThatFits(rect.size).height
             contentLabel.frame = rect
-            if contentLabel.intrinsicContentSize.width < rect.width {
-                rect.size.width = contentLabel.intrinsicContentSize.width
-                contentLabel.frame = rect
+            rect.size.width = min(rect.width, contentLabel.intrinsicContentSize.width)
+            rect.origin.x += spacing
+            contentLabel.frame = rect
+            
+            rect.size.width += (spacing * 2)
+            rect.size.height += (spacing * 2)
+            rect.origin.y -= spacing
+            rect.origin.x -= spacing
+            contentBGView.frame = rect
+            
+            rect.origin.y = rect.maxY
+            
+            if timeLabel.isHidden {
+                rect.size = .zero
+                
+            } else {
+                rect.size.width = bounds.width - rect.origin.x - avatarImageView.frame.origin.x
+                rect.size.height = timeLabel.sizeThatFits(rect.size).height
+                rect.origin.y += spacing
             }
             
-            rect.origin.y -= (spacing * 2)
-            rect.origin.x -= (spacing * 2)
-            rect.size.height += (spacing * 4)
-            rect.size.width += (spacing * 4)
-            contentBGView.frame = rect
+            timeLabel.frame = rect
             
         case ReuseID.right.rawValue:
-            contentBGView.backgroundColor = .orange
-            
-            rect.size.width = bounds.width * 0.8 - spacing * 4
-            rect.origin.x = bounds.width - rect.width - spacing * 4
+            rect.size.width = bounds.width * 0.8 - spacing * 2
             rect.size.height = contentLabel.sizeThatFits(rect.size).height
+            rect.origin.y = spacing * 2
             contentLabel.frame = rect
-            if contentLabel.intrinsicContentSize.width < rect.width {
-                rect.size.width = contentLabel.intrinsicContentSize.width
-                rect.origin.x = bounds.width - contentLabel.intrinsicContentSize.width - spacing * 4
-                contentLabel.frame = rect
+            rect.size.width = min(rect.width, contentLabel.intrinsicContentSize.width)
+            rect.origin.x = bounds.width - rect.width - spacing * 2
+            contentLabel.frame = rect
+            
+            rect.size.width += (spacing * 2)
+            rect.size.height = rect.maxY
+            rect.origin.y = spacing
+            rect.origin.x = bounds.width - rect.width - spacing
+            contentBGView.frame = rect
+         
+            rect.origin.y = rect.maxY
+            
+            if timeLabel.isHidden {
+                rect.size = .zero
+                
+            } else {
+                rect.size.width = bounds.width * 0.8 - spacing * 2
+                rect.size.height = timeLabel.sizeThatFits(rect.size).height
+                rect.origin.y += spacing
+                rect.origin.x = bounds.width - rect.width - spacing
             }
             
-            rect.origin.y -= (spacing * 2)
-            rect.origin.x -= (spacing * 2)
-            rect.size.height += (spacing * 4)
-            rect.size.width += (spacing * 4)
-            contentBGView.frame = rect
+            timeLabel.frame = rect
             
         default:
             break
