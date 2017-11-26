dbbc09a3bf5c80c8d69ede0b9d85171511e24208
Mounir Ybanez <rinuom91@gmail.com>
2017-11-26 16:07:13 +0800

    Added composer view.

---
+++ Chika/Source/Scenes/Convo/ConvoSceneComposerView.swift
@@ -0,0 +1,125 @@
+//
+//  ConvoSceneComposerView.swift
+//  Chika
+//
+//  Created by Mounir Ybanez on 11/25/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol ConvoSceneComposerViewInteraction: class {
+    
+    func contentDidChangeText(notif: NSNotification)
+    func didTapPlaceholder()
+}
+
+class ConvoSceneComposerView: UIView {
+    
+    var strip: UIView!
+    var sendButton: UIButton!
+    var contentInput: UITextView!
+    var placeholderLabel: UILabel!
+    
+    deinit {
+        removeContentObserver()
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+        initSetup()
+    }
+    
+    func initSetup() {
+        strip = UIView()
+        
+        sendButton = UIButton()
+        sendButton.setTitle("Send", for: .normal)
+        
+        contentInput = UITextView()
+        
+        placeholderLabel = UILabel()
+        placeholderLabel.text = "Write message"
+        placeholderLabel.isUserInteractionEnabled = true
+        
+        addPlaceholderTap()
+        addContentObserver()
+        
+        addSubview(strip)
+        addSubview(sendButton)
+        addSubview(contentInput)
+        addSubview(placeholderLabel)
+    }
+    
+    override func layoutSubviews() {
+        var rect = CGRect.zero
+        
+        let spacing: CGFloat = 8
+        
+        rect.size.width = bounds.width
+        rect.size.height = 1
+        strip.frame = rect
+        
+        rect.size.width = 44
+        rect.size.height = rect.width
+        rect.origin.x = bounds.width - rect.width - spacing
+        rect.origin.y = (bounds.height - rect.height - safeAreaInsets.bottom) / 2
+        sendButton.frame = rect
+        
+        rect.origin.y = spacing
+        rect.origin.x = spacing
+        rect.size.width = bounds.width - sendButton.frame.width - spacing * 2
+        rect.size.height = bounds.height - spacing * 2 - safeAreaInsets.bottom
+        contentInput.frame = rect
+        
+        rect.size = placeholderLabel.sizeThatFits(rect.size)
+        rect.origin.x += contentInput.textContainerInset.left
+        rect.origin.x += contentInput.textContainer.lineFragmentPadding
+        rect.origin.y += contentInput.textContainerInset.top
+        placeholderLabel.frame = rect
+    }
+    
+    func updateContent(_ content: String) {
+        contentInput.text = content
+        NotificationCenter.default.post(
+            name: NSNotification.Name.UITextViewTextDidChange,
+            object: contentInput)
+    }
+    
+    func addContentObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.contentDidChangeText),
+            name: NSNotification.Name.UITextViewTextDidChange,
+            object: nil)
+    }
+    
+    func removeContentObserver() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: NSNotification.Name.UITextViewTextDidChange,
+            object: nil)
+    }
+    
+    func addPlaceholderTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.didTapPlaceholder))
+        tap.numberOfTapsRequired = 1
+        placeholderLabel.addGestureRecognizer(tap)
+    }
+}
+
+extension ConvoSceneComposerView: ConvoSceneComposerViewInteraction {
+    
+    func contentDidChangeText(notif: NSNotification) {
+        guard let textView = notif.object as? UITextView,
+            textView == contentInput, contentInput.isFirstResponder else {
+                return
+        }
+        
+        placeholderLabel.isHidden = !contentInput.text.isEmpty
+    }
+    
+    func didTapPlaceholder() {
+        contentInput.becomeFirstResponder()
+    }
+}
