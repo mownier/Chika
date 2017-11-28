b846ba0867547b5e41e9170873f5949b7bddcc68
Mounir Ybanez <rinuom91@gmail.com>
2017-11-29 00:18:38 +0800

    Added mock for chat message remote listener.

---
+++ ChikaTests/Mocks/ChatMessageRemoteListenerMock.swift
@@ -0,0 +1,16 @@
+//
+//  ChatMessageRemoteListenerMock.swift
+//  ChikaTests
+//
+//  Created by Mounir Ybanez on 11/28/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+@testable import Chika
+
+class ChatMessageRemoteListenerMock: ChatMessageRemoteListener {
+    
+    func listen(callback: @escaping (Message) -> Void) {
+        
+    }
+}
