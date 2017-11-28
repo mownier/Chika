1ec267a64cc8f6016a80b9947f531e6a8dbc071b
Mounir Ybanez <rinuom91@gmail.com>
2017-11-29 00:18:52 +0800

    Added mock for inbox remote listener.

---
+++ ChikaTests/Mocks/InboxRemoteListenerMock.swift
@@ -0,0 +1,16 @@
+//
+//  InboxRemoteListenerMock.swift
+//  ChikaTests
+//
+//  Created by Mounir Ybanez on 11/28/17.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+@testable import Chika
+
+class InboxRemoteListenerMock: InboxRemoteListener {
+    
+    func listen(callback: @escaping (Chat) -> Void) {
+        
+    }
+}
