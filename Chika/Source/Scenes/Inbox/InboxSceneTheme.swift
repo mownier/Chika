17319a29533dac3a0a71b60b1f5bd010f41cce17
Mounir Ybanez <rinuom91@gmail.com>
2017-12-01 15:31:18 +0800

    Added colors and font.

--- Chika/Source/Scenes/Inbox/InboxSceneTheme.swift
+++ Chika/Source/Scenes/Inbox/InboxSceneTheme.swift
@@ -12,8 +12,20 @@ protocol InboxSceneTheme: class {
 
     var bgColor: UIColor { get }
     
-    var chatTitleLabelFont: UIFont? { get }
-    var chatRecentMessageLabelFont: UIFont? { get }
+    var chatTitleFont: UIFont? { get }
+    var chatTitleTextColor: UIColor { get }
+    var chatRecentMessageFont: UIFont? { get }
+    var chatRecentMessageTextColor: UIColor { get }
+    var chatTimeFont: UIFont? { get }
+    var chatTimeTextColor: UIColor { get }
+    
+    var unreadMessaegCountFont: UIFont? { get }
+    var unreadMessageCountTextColor: UIColor { get }
+    var unreadMessageBGColor: UIColor { get }
+    
+    var onlineStatusColor: UIColor { get }
+    
+    var stripColor: UIColor { get }
 }
 
 extension InboxScene {
@@ -21,7 +33,20 @@ extension InboxScene {
     class Theme: InboxSceneTheme {
         
         var bgColor: UIColor = .white
-        var chatTitleLabelFont: UIFont? = UIFont(name: "AvenirNext-Bold", size: 16.0)
-        var chatRecentMessageLabelFont: UIFont? = UIFont(name: "AvenirNext-Regular", size: 14.0)
+        
+        var chatTitleFont: UIFont? = UIFont(name: "AvenirNext-Bold", size: 16.0)
+        var chatTitleTextColor: UIColor = .black
+        var chatRecentMessageFont: UIFont? = UIFont(name: "AvenirNext-Regular", size: 14.0)
+        var chatRecentMessageTextColor: UIColor = .gray
+        var chatTimeFont: UIFont? = UIFont(name: "AvenirNext-Bold", size: 12.0)
+        var chatTimeTextColor: UIColor = UIColor.gray
+        
+        var unreadMessaegCountFont: UIFont? = UIFont(name: "AvenirNext-Bold", size: 12.0)
+        var unreadMessageCountTextColor: UIColor  = .white
+        var unreadMessageBGColor: UIColor = UIColor(red: 0 / 255, green: 122 / 255, blue: 255 / 255, alpha: 1.0)
+        
+        var onlineStatusColor: UIColor = UIColor(red: 76 / 255, green: 217 / 255, blue: 100 / 255, alpha: 1.0)
+        
+        var stripColor: UIColor = UIColor.gray.withAlphaComponent(0.25)
     }
 }
