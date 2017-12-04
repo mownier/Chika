5b194a3b2fd0f18268c3a2b9ad2de49e52cbd794
Mounir Ybanez <rinuom91@gmail.com>
2017-12-04 15:17:32 +0800

    Used presence writer.

--- Chika/Source/Application/AppDelegate.swift
+++ Chika/Source/Application/AppDelegate.swift
@@ -13,13 +13,26 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var presenceWriter: PresenceRemoteWriter!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        presenceWriter = PresenceRemoteWriterProvider()
         
         let waypoint = RootWaypoint()
         let _ = waypoint.makeRoot(from: window)
         
         return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        guard presenceWriter != nil else { return }
+        presenceWriter.makeOnline { _ in }
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        guard presenceWriter != nil else { return }
+        presenceWriter.makeOffline { _ in }
     }
 }
