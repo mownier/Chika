4502dc728855975518ce78e2945700db4fe7c97a
Mounir Ybanez <rinuom91@gmail.com>
2017-12-04 15:21:35 +0800

    Added did recive recent chat, active and typing status changes.

--- Chika/Source/Scenes/Inbox/InboxSceneWorker.swift
+++ Chika/Source/Scenes/Inbox/InboxSceneWorker.swift
@@ -10,7 +10,11 @@ import FirebaseAuth
 
 protocol InboxSceneWorker: class {
 
-    func listenForInboxUpdates()
+    func listenForRecentChat()
+    func listenOnActiveStatus(for chat: Chat?)
+    func listenOnTypingStatus(for chat: Chat?)
+    func unlistenOnActiveStatus(for chat: Chat?)
+    func unlistenOnTypingStatus(for chat: Chat?)
     func fetchInbox()
 }
 
@@ -18,33 +22,44 @@ protocol InboxSceneWorkerOutput: class {
     
     func workerDidFetch(chats: [Chat])
     func workerDidFetchWithError(_ error: Error)
-    func workerDidUpdateInbox(chat: Chat)
+    func workerDidReceiveRecentChat(_ chat: Chat)
+    func workerDidChangeActiveStatus(for participantID: String, isActive: Bool)
+    func workerDidChangeTypingStatus(for chatID: String, participantID: String, isTyping: Bool)
 }
 
 extension InboxScene {
     
     class Worker: InboxSceneWorker {
         
-        weak var output: InboxSceneWorkerOutput?
-        var personID: String
-        var service: ChatRemoteService
-        var listener: InboxRemoteListener
+        struct Listener {
+            
+            var inbox: InboxRemoteListener
+            var presence: PresenceRemoteListener
+            var typingStatus: TypingStatusRemoteListener
+        }
         
-        init(personID: String, service: ChatRemoteService, listener: InboxRemoteListener) {
-            self.personID = personID
+        weak var output: InboxSceneWorkerOutput?
+        var meID: String
+        var service: ChatRemoteService
+        var listener: Listener
+        
+        init(meID: String, service: ChatRemoteService, listener: Listener) {
+            self.meID = meID
             self.service = service
             self.listener = listener
         }
         
-        convenience init(user: User? = Auth.auth().currentUser) {
-            let personID = user?.uid ?? ""
+        convenience init(meID: String = Auth.auth().currentUser?.uid ?? "") {
             let service = ChatRemoteServiceProvider()
-            let listener = InboxRemoteListenerProvider(personID: personID)
-            self.init(personID: personID, service: service, listener: listener)
+            let inbox = InboxRemoteListenerProvider(meID: meID)
+            let presence = PresenceRemoteListenerProvider()
+            let typingStatus = TypingStatusRemoteListenerProvider()
+            let listener = Listener(inbox: inbox, presence: presence, typingStatus: typingStatus)
+            self.init(meID: meID, service: service, listener: listener)
         }
         
         func fetchInbox() {
-            service.getInbox(for: personID) { [weak self] result in
+            service.getInbox(for: meID) { [weak self] result in
                 switch result {
                 case .err(let info):
                     self?.output?.workerDidFetchWithError(info)
@@ -55,10 +70,50 @@ extension InboxScene {
             }
         }
         
-        func listenForInboxUpdates() {
-            listener.listen { [weak self] chat in
-                self?.output?.workerDidUpdateInbox(chat: chat)
+        func listenForRecentChat() {
+            listener.inbox.listenForRecentChat { [weak self] chat in
+                self?.output?.workerDidReceiveRecentChat(chat)
             }
+        }
+        
+        func listenOnActiveStatus(for chat: Chat?) {
+            guard chat != nil, !chat!.participants.isEmpty else {
+                return
+            }
+            
+            for participant in chat!.participants {
+                let _ = listener.presence.listen(personID: participant.id) { [weak self] isActive in
+                    self?.output?.workerDidChangeActiveStatus(for: participant.id, isActive: isActive)
+                }
+            }
+        }
+        
+        func listenOnTypingStatus(for chat: Chat?) {
+            guard let chat = chat else {
+                return
+            }
+            
+            let _ = listener.typingStatus.listen(for: chat.id) { [weak self] personID, isTyping in
+                self?.output?.workerDidChangeTypingStatus(for: chat.id, participantID: personID, isTyping: isTyping)
+            }
+        }
+        
+        func unlistenOnActiveStatus(for chat: Chat?) {
+            guard chat != nil, !chat!.participants.isEmpty else {
+                return
+            }
+            
+            for participant in chat!.participants {
+                let _ = listener.presence.unlisten(personID: participant.id)
+            }
+        }
+        
+        func unlistenOnTypingStatus(for chat: Chat?) {
+            guard let chat = chat else {
+                return
+            }
+            
+            let _ = listener.typingStatus.unlisten(for: chat.id)
         }
     }
 }
