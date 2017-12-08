0a58be17b03193a1cd3d536e08254a4d36bb39ea
Mounir Ybanez <rinuom91@gmail.com>
2017-12-09 01:07:24 +0800

    Added getting of profile.

--- Chika/Source/Services/Remote/PersonRemoteService.swift
+++ Chika/Source/Services/Remote/PersonRemoteService.swift
@@ -7,18 +7,25 @@
 //
 
 import FirebaseDatabase
+import FirebaseAuth
 
 protocol PersonRemoteService: class {
     
     func add(email: String, id: String, completion: @escaping (ServiceResult<String>) -> Void)
+    func getProfile(of personID: String, completion: @escaping(ServiceResult<Person>) -> Void)
+    func getMyProfile(completion: @escaping(ServiceResult<Person>) -> Void)
 }
 
 class PersonRemoteServiceProvider: PersonRemoteService {
 
     var database: Database
+    var personsQuery: PersonsRemoteQuery
+    var meID: String
     
-    init(database: Database = Database.database()) {
+    init(meID: String = Auth.auth().currentUser?.uid ?? "", database: Database = Database.database(), personsQuery: PersonsRemoteQuery = PersonsRemoteQueryProvider()) {
+        self.meID = meID
         self.database = database
+        self.personsQuery = personsQuery
     }
     
     func add(email: String, id: String, completion: @escaping (ServiceResult<String>) -> Void) {
@@ -32,5 +39,27 @@ class PersonRemoteServiceProvider: PersonRemoteService {
             
             completion(.ok("OK"))
         }
+    }
+    
+    func getProfile(of personID: String, completion: @escaping (ServiceResult<Person>) -> Void) {
+        guard !personID.isEmpty else {
+            completion(.err(ServiceError("person ID is empty")))
+            return
+        }
+        
+        personsQuery.getPersons(for: [personID]) { persons in
+            let persons = Array(Set(persons.filter({ $0.id == personID })))
+            
+            guard !persons.isEmpty else {
+                completion(.err(ServiceError("person not found")))
+                return
+            }
+            
+            completion(.ok(persons[0]))
+        }
+    }
+    
+    func getMyProfile(completion: @escaping (ServiceResult<Person>) -> Void) {
+        getProfile(of: meID, completion: completion)
     }
 }
