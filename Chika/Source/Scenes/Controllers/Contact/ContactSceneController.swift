8d6d19d491eed87941da8092eda37308d5929760
Mounir Ybanez <rinuom91@gmail.com>
2017-12-13 21:40:54 +0800

    Implemented PeopleSearchScene delegate.

--- Chika/Source/Scenes/Controllers/Contact/ContactSceneController.swift
+++ Chika/Source/Scenes/Controllers/Contact/ContactSceneController.swift
@@ -8,24 +8,12 @@
 
 import UIKit
 
-@objc protocol ContactSceneControllerInteraction: class {
-    
-    func didTapCancel()
-    func didStartSearching(_ notif: Notification)
-}
-
 class ContactSceneController: UIViewController {
 
     weak var contactsView: UIView!
     weak var peopleSearchView: UIView!
-    weak var input: PeopleSearchSceneInput!
     
     var factory: SubsceneFactory
-    var notifCenter: NotificationCenter = .default
-    
-    deinit {
-        notifCenter.removeObserver(self)
-    }
     
     init(factory: SubsceneFactory) {
         self.factory = factory
@@ -46,14 +34,11 @@ class ContactSceneController: UIViewController {
     override func loadView() {
         super.loadView()
         
-        view.backgroundColor = .red
-        
-        let peopleSearch = factory.peopleSearch.build() as! PeopleSearchScene
-        addChildViewController(peopleSearch)
-        peopleSearch.didMove(toParentViewController: self)
-        
+        let peopleSearch = factory.peopleSearch.build(withDelegate: self)
         let contacts = factory.contacts.build()
+        addChildViewController(peopleSearch)
         addChildViewController(contacts)
+        peopleSearch.didMove(toParentViewController: self)
         contacts.didMove(toParentViewController: self)
         
         contactsView = contacts.view
@@ -61,10 +46,6 @@ class ContactSceneController: UIViewController {
         
         view.addSubview(peopleSearchView)
         view.addSubview(contactsView)
-        
-        input = peopleSearch.input
-        input.cancelButton.addTarget(self, action: #selector(self.didTapCancel), for: .touchUpInside)
-        notifCenter.addObserver(self, selector: #selector(self.didStartSearching(_:)), name: .UITextFieldTextDidBeginEditing, object: nil)
     }
     
     override func viewDidLayoutSubviews() {
@@ -73,21 +54,20 @@ class ContactSceneController: UIViewController {
         rect = view.bounds
         peopleSearchView.frame = rect
     
-        rect.origin.y = input.frame.height
+        rect.origin.y = 59 + view.statusBarFrame().height
         rect.size.height = view.bounds.height - rect.minY
         contactsView.frame = rect
     }
 }
 
-extension ContactSceneController: ContactSceneControllerInteraction {
+extension ContactSceneController: PeopleSearchSceneDelegate {
     
-    func didTapCancel() {
-        view.bringSubview(toFront: contactsView)
+    func peopleSearchSceneDidBeginSearching() {
+        view.sendSubview(toBack: contactsView)
     }
     
-    func didStartSearching(_ notif: Notification) {
-        guard notif.object as? UITextField == input.searchInput else { return }
-        view.sendSubview(toBack: contactsView)
+    func peopleSearchSceneDidEndSearching() {
+        view.bringSubview(toFront: contactsView)
     }
 }
 
