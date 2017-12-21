3135a43a64df64dd67d627e238d3b4430174ecaf
Mounir Ybanez <rinuom91@gmail.com>
2017-12-21 23:00:46 +0800

    Removed unsure test.

--- ChikaTests/Services/Remote/ChatsRemoteServiceProviderTests.swift
+++ ChikaTests/Services/Remote/ChatsRemoteServiceProviderTests.swift
@@ -34,37 +34,4 @@ class ChatsRemoteServiceProviderTests: XCTestCase {
         }
         wait(for: [exp], timeout: 2.0)
     }
-    
-    // CONTEXT: getInbox function should
-    //   - return a reversed array of Chat
-    // GIVEN:
-    //   - personID is not empty
-    //   - inboxQuery returns a non-empty array
-    func testGetInboxB() {
-        let exp = expectation(description: "testGetInboxB")
-        let inboxQuery = InboxRemoteQueryMock()
-        let service = ChatRemoteServiceProvider(inboxQuery: inboxQuery)
-        let personID = "person:1"
-        var chat1 = Chat()
-        var chat2 = Chat()
-        var chat3 = Chat()
-        chat1.id = "chat:1"
-        chat2.id = "chat:2"
-        chat3.id = "chat:3"
-        let mockChats: [Chat] = [chat1, chat2, chat3]
-        inboxQuery.mockChats = mockChats
-        service.getInbox(for: personID) { result in
-            switch result {
-            case .err:
-                XCTFail()
-            
-            case .ok(let chats):
-                let reversed: [Chat] = mockChats.reversed()
-                let info: [Chat] = chats
-                XCTAssertEqual(reversed.map({ $0.id }), info.map({ $0.id }))
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 2.0)
-    }
 }
